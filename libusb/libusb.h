81204eed3d0a7744b89b68bd90d567b68d191a91
Daniel Drake <dsd@gentoo.org>
2008-05-02 17:45:23 +0100

    document pollfd notifiers

--- libusb/libusb.h
+++ libusb/libusb.h
@@ -729,9 +729,6 @@ static inline void libusb_fill_control_setup(unsigned char *buffer,
 	setup->wLength = wLength;
 }
 
-size_t libusb_get_transfer_alloc_size(void);
-void libusb_init_transfer(struct libusb_transfer *transfer);
-
 struct libusb_transfer *libusb_alloc_transfer(int iso_packets);
 int libusb_submit_transfer(struct libusb_transfer *transfer);
 int libusb_cancel_transfer(struct libusb_transfer *transfer);
@@ -902,7 +899,23 @@ int libusb_handle_events(void);
 int libusb_get_next_timeout(struct timeval *tv);
 const struct libusb_pollfd **libusb_get_pollfds(void);
 
+/** \ingroup poll
+ * Callback function, invoked when a new file descriptor should be added
+ * to the set of file descriptors monitored for events.
+ * \param fd the new file descriptor
+ * \param events events to monitor for, see \ref libusb_pollfd for a
+ * description
+ * \see libusb_set_pollfd_notifiers
+ */
 typedef void (*libusb_pollfd_added_cb)(int fd, short events);
+
+/** \ingroup poll
+ * Callback function, invoked when a file descriptor should be removed from
+ * the set of file descriptors being monitored for events. After returning
+ * from this callback, do not use that file descriptor again.
+ * \param fd the file descriptor to stop monitoring
+ * \see libusb_set_pollfd_notifiers
+ */
 typedef void (*libusb_pollfd_removed_cb)(int fd);
 void libusb_set_pollfd_notifiers(libusb_pollfd_added_cb added_cb,
 	libusb_pollfd_removed_cb removed_cb);
