3b33bade3909ed83d566238e0e00d02ca376d18a
Pete Batard <pbatard@gmail.com>
2010-03-08 00:40:31 +0000

    fix libtool issue when building shared libs with Chinese locale
    This patch modifies the ltmain.sh (and therefore libtool) scripts to
    force the locale to C, even if not defined.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,12 @@
 (glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
 
 $LIBTOOLIZE --copy --force || exit 1
+# Force ltmain's NLS test to set locale to C always. Prevents an
+# issue when compiling shared libs with MinGW on Chinese locale.
+type -P sed &>/dev/null || { echo "sed command not found. Aborting." >&2; exit 1; }
+sed -e s/\\\\\${\$lt_var+set}/set/g ltmain.sh > lttmp.sh
+mv lttmp.sh ltmain.sh
+#
 aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
