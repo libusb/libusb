26a0c962b13088896a91052e78a23303aa085260
Pete Batard <pbatard@gmail.com>
2010-04-13 23:29:27 +0100

    fixed the use of internal OS_WINDOWS in general file libusb.h
    OS_WINDOWS is only defined in the context of compiling libusb
    switched to using the explicit MinGW/cygwin and MS compiler defines

--- libusb/libusb.h
+++ libusb/libusb.h
@@ -36,7 +36,7 @@
  * libusb_config_descriptor has an 'interface' member
  * As this can be problematic if you include windows.h after libusb.h
  * in your sources, we force windows.h to be included first. */
-#if defined(OS_WINDOWS) && !defined(_WINDOWS_)
+#if (defined(_MSC_VER) || defined(__MINGW32__) || defined(__CYGWIN__)) && !defined(_WINDOWS_)
 #include <windows.h>
 #endif
 
