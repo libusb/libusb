b7c99f781f34103174b368dd22502b56c572be0c
Pete Batard <pbatard@gmail.com>
2010-04-19 13:26:31 +0100

    added official libtool NLS patch for Chinese locale

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-
 # use glibtoolize if it is available (darwin)
 (glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
 
 $LIBTOOLIZE --copy --force || exit 1
-# Force ltmain's NLS test to set locale to C always. Prevents an
-# issue when compiling shared libs with MinGW on Chinese locale.
-type -P sed &>/dev/null || { echo "sed command not found. Aborting." >&2; exit 1; }
-sed -e s/\\\\\${\$lt_var+set}/set/g ltmain.sh > lttmp.sh
-mv lttmp.sh ltmain.sh
+# If available, apply libtool's NLS patch to set locale to C always.
+# Prevents an issue when compiling shared libs with MinGW on Chinese locale.
+# see: http://lists.gnu.org/archive/html/bug-libtool/2010-03/msg00012.html
+type -P patch &>/dev/null && { if [ -e "libtool-nls.diff" ]; then patch -p1 -Nl -i libtool-nls.diff &>/dev/null; fi; }
 #
 aclocal || exit 1
 autoheader || exit 1
