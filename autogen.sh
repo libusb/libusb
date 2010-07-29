5eb857963eb6e16f461abaef0ee83bbca6bc663b
Pete Batard <pbatard@gmail.com>
2010-07-29 23:05:17 +0100

    fixed residuals from official: autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # use glibtoolize if it is available (darwin)
 (glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
 
@@ -12,4 +13,5 @@ aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
-./configure --enable-debug-log --enable-examples-build $*
+./configure --enable-maintainer-mode --enable-debug-log \
+	--enable-examples-build $*
