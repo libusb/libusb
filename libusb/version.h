85a14f43e0ce27cd47f84fdaeacbaa81cebdb886
Peter Stuge <peter@stuge.se>
2011-05-09 08:12:24 +0200

    Move library version number from configure.ac to libusb/version.h
    This is neccessary to support native MS builds. The Windows resource
    file libusb/libusb-1.0.rc must include the release version, which was
    previously only available after configure had run and had substituted
    the numbers into a generated libusb/libusb-1.0.rc file.
    
    The version atoms are now stored as CPP style #defines in libusb/version.h
    so that the .rc no longer needs to be generated but can simply include the
    header file and access the version information directly.
    
    The m4 macro LU_DEFINE_VERSION_ATOM() was added to configure.ac to get
    version atoms from libusb/version.h for use in AC_INIT(). The macro
    handles C and C++ style comments in version.h, but can easily be made
    to fail by obscuring the file. Please don't do that.
    
    Tested with MinGW using autoconf, and manual compile of libusb-1.0.rc
    using RC.EXE Version 5.2.3690.0 from Visual C++ 2005 Express Edition.

---
+++ libusb/version.h
@@ -0,0 +1,14 @@
+/* This file is parsed by m4 and windres and RC.EXE so please keep it simple. */
+#ifndef LIBUSB_MAJOR
+#define LIBUSB_MAJOR 1
+#endif
+#ifndef LIBUSB_MINOR
+#define LIBUSB_MINOR 0
+#endif
+#ifndef LIBUSB_MICRO
+#define LIBUSB_MICRO 8
+#endif
+/* LIBUSB_NANO may be used for Windows internal versioning. 0 means unused. */
+#ifndef LIBUSB_NANO
+#define LIBUSB_NANO 0
+#endif
