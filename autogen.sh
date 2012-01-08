214507c5d94458a841bcab2ddc8c75d762b05d6a
Pete Batard <pete@akeo.ie>
2012-01-08 19:52:55 +0000

    [revert] revert Chinese NLS libtool patch
    * see r265 = b7c99f781f34103174b368dd22502b56c572be0c
    * more than 18 months have elapsed since the fix has
      been integrated in libtool => most people should have
      upgraded by now

--- autogen.sh
+++ autogen.sh
@@ -27,11 +27,6 @@ create_def()
 (glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
 
 $LIBTOOLIZE --copy --force || exit 1
-# If available, apply libtool's NLS patch to set locale to C always.
-# Prevents an issue when compiling shared libs with MinGW on Chinese locale.
-# see: http://lists.gnu.org/archive/html/bug-libtool/2010-03/msg00012.html
-type -P patch &>/dev/null && { if [ -e "libtool-nls.diff" ]; then patch -p1 -Nl -i libtool-nls.diff &>/dev/null; fi; }
-#
 aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
