3df437680f8f5406795ec5f32d96612d327b2ca4
Peter Stuge <peter@stuge.se>
2012-02-10 18:06:43 +0100

    autogen.sh: Try to use libtoolize before trying glibtoolize
    This helps on Mac OS X where an old glibtoolize is included in the
    system and newer, manually installed, versions provide libtoolize.
    
    See also http://marc.info/?m=132490560131894

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-# use glibtoolize if it is available (darwin)
-(glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
+# use libtoolize if available, otherwise look for glibtoolize (darwin)
+if (libtoolize --version) < /dev/null > /dev/null 2>&1; then
+  LIBTOOLIZE=libtoolize
+elif (glibtoolize --version) < /dev/null > /dev/null 2>&1; then
+  LIBTOOLIZE=glibtoolize
+else
+  echo "libtoolize or glibtoolize was not found! Please install libtool."
+  exit
+fi
 
 $LIBTOOLIZE --copy --force || exit 1
 aclocal || exit 1
