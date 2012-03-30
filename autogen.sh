dbd6faf6af3e6a0d0e5a238549d96630023591cb
Pete Batard <pete@akeo.ie>
2012-03-31 00:21:18 +0100

    Autotools: remove --enable-debug-log option in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -15,5 +15,4 @@ aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
-./configure --enable-maintainer-mode --enable-debug-log \
-	--enable-examples-build $*
+./configure --enable-maintainer-mode --enable-examples-build $*
