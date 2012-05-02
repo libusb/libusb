ae1124c112c88e807fb5f500846c9b12ea882122
Pete Batard <pete@akeo.ie>
2012-05-02 12:06:36 +0100

    Autotools: Add bootstrap.sh
    bootstrap.sh does not invoke configure whereas autogen.sh does.
    This allows libusbx users to choose the one that suits them best.

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# use libtoolize if available, otherwise look for glibtoolize (darwin)
+if (libtoolize --version) < /dev/null > /dev/null 2>&1; then
+  LIBTOOLIZE=libtoolize
+elif (glibtoolize --version) < /dev/null > /dev/null 2>&1; then
+  LIBTOOLIZE=glibtoolize
+else
+  echo "libtoolize or glibtoolize was not found! Please install libtool."
+  exit
+fi
+
+$LIBTOOLIZE --copy --force || exit 1
+aclocal || exit 1
+autoheader || exit 1
+autoconf || exit 1
+automake -a -c || exit 1
