6b7609125f02b18a3b57a2b101d1404aab8f181e
Ludovic Rousseau <ludovic.rousseau@gmail.com>
2012-05-09 09:12:01 +0200

    Samples: Use a common LDADD for all examples
    * Similar to libusb patch a129732fb45fc424743f26f01c906e4514f11521

--- libusb/version.h
+++ libusb/version.h
@@ -9,7 +9,7 @@
 #define LIBUSB_MICRO 11
 #endif
 #ifndef LIBUSB_NANO
-#define LIBUSB_NANO 10500
+#define LIBUSB_NANO 10501
 #endif
 /* LIBUSB_RC is the release candidate suffix. Should normally be empty. */
 #ifndef LIBUSB_RC
