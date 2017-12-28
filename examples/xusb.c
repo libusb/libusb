f66c63e9f0ca621e1ee17ed5f5855f1b29f031dc
Sean McBride <sean@rogue-research.com>
2017-12-27 22:34:22 -0500

    examples: xusb: replaced insecure sprintf with snprintf
    Signed-off-by: Chris Dickens <christopher.a.dickens@gmail.com>

--- examples/xusb.c
+++ examples/xusb.c
@@ -183,7 +183,8 @@ static char* uuid_to_string(const uint8_t* uuid)
 {
 	static char uuid_string[40];
 	if (uuid == NULL) return NULL;
-	sprintf(uuid_string, "{%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x}",
+	snprintf(uuid_string, sizeof(uuid_string),
+		"{%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x}",
 		uuid[0], uuid[1], uuid[2], uuid[3], uuid[4], uuid[5], uuid[6], uuid[7],
 		uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
 	return uuid_string;
--- libusb/version_nano.h
+++ libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11235
+#define LIBUSB_NANO 11236
