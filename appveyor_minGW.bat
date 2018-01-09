ca0b6c09fac0d9144b5397cbf6cca3615ed085eb
Chris Dickens <christopher.a.dickens@gmail.com>
2018-01-08 20:41:50 -0800

    Fix appveyor scripts for Cygwin and MinGW to build as intended
    The sub-directory created to store the generated files and build objects
    was not being used as intended, because each invocation of the bash
    starts in the home directory. Fix this by changing to the intended
    directory prior to each command invocation.
    
    Also replace the invocation of autogen.sh with bootstrap.sh, since using
    autogen.sh calls ./configure and we weren't using the results of that
    work. Instead we call configure directly from the sub-directory while
    enabling the examples and test builds (for Cygwin) as autogen.sh would do.
    
    Also fix the invalid value for the --build option provided to the x64
    build of MinGW.
    
    Finally, propagate errors from each script command so that failures are
    caught and reported.
    
    Signed-off-by: Chris Dickens <christopher.a.dickens@gmail.com>

--- appveyor_minGW.bat
+++ appveyor_minGW.bat
@@ -1,24 +1,19 @@
 echo on
 SetLocal EnableDelayedExpansion
 
-if [%Configuration%] NEQ [Debug] goto releasex64
-
-:releasex64
-if [%Platform%] NEQ [x64] goto releaseWin32
 if [%Configuration%] NEQ [Release] exit 0
-C:\msys64\usr\bin\bash -e -l -c "mkdir build-x64"
-C:\msys64\usr\bin\bash -e -l -c ./autogen.sh
-C:\msys64\usr\bin\bash -e -l -c "cd build-x64"
-C:\msys64\usr\bin\bash -e -l -c "build-x64/../configure --prefix=/mingw64 --build=--build= --host=x86_64-w64-mingw32"
-C:\msys64\usr\bin\bash -e -l -c "make -j4"
-C:\msys64\usr\bin\bash -e -l -c "make install"
 
-:releaseWin32
+if [%Platform%] NEQ [x64] goto Win32
+C:\msys64\usr\bin\bash -e -l -c "./bootstrap.sh" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "mkdir build-x64" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "cd build-x64 && ../configure --prefix=/mingw64 --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "cd build-x64 && make -j4" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "cd build-x64 && make install" || exit /B
+
+:Win32
 if [%Platform%] NEQ [Win32] exit 0
-if [%Configuration%] NEQ [Release] exit 0
-C:\msys64\usr\bin\bash -e -l -c "mkdir build-Win32"
-C:\msys64\usr\bin\bash -e -l -c ./autogen.sh
-C:\msys64\usr\bin\bash -e -l -c "cd build-Win32"
-C:\msys64\usr\bin\bash -e -l -c "build-Win32/../configure --prefix=/mingw32 --build=i686-w64-mingw32 --host=i686-w64-mingw32"
-C:\msys64\usr\bin\bash -e -l -c "make -j4"
-C:\msys64\usr\bin\bash -e -l -c "make install"
+C:\msys64\usr\bin\bash -e -l -c "./bootstrap.sh" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "mkdir build-Win32" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "cd build-Win32 && ../configure --prefix=/mingw32 --build=i686-w64-mingw32 --host=i686-w64-mingw32" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "cd build-Win32 && make -j4" || exit /B
+C:\msys64\usr\bin\bash -e -l -c "cd build-Win32 && make install" || exit /B
--- libusb/version_nano.h
+++ libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11285
+#define LIBUSB_NANO 11286
