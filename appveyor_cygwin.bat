364d8b6d7ca6212277538f4a26c807cd473d07a5
Chris Dickens <christopher.a.dickens@gmail.com>
2018-01-08 20:41:50 -0800

    Fix appveyor scripts for Cygwin and MinGW to build as intended
    The sub-directory created to store the generated files and build objects
    was not being used as intended, because each invocation of the bash
    starts in the home directory. Fix this by changing to the intended
    directory prior to each command invocation.
    
    Also replace the invocation of autogen.sh with bootstrap.sh, since using
    autogen.sh calls ./configure and we weren't using the results of that
    work. Instead we call configure directly from the sub-directory while
    enabling the examples and test builds (for Cygwin) as autogen.sh would do.
    
    Also fix the invalid value for the --build option provided to the x64
    build of MinGW.
    
    Finally, propagate errors from each script command so that failures are
    caught and reported.
    
    Signed-off-by: Chris Dickens <christopher.a.dickens@gmail.com>

--- appveyor_cygwin.bat
+++ appveyor_cygwin.bat
@@ -1,14 +1,11 @@
 echo on
 SetLocal EnableDelayedExpansion
 
-if [%Configuration%] NEQ [Debug] goto releaseWin32
-
-:releaseWin32
-if [%Platform%] NEQ [Win32] exit 0
 if [%Configuration%] NEQ [Release] exit 0
-C:\cygwin\bin\bash -e -l -c "mkdir build-Win32-cygwin"
-C:\cygwin\bin\bash -e -l -c ./autogen.sh
-C:\cygwin\bin\bash -e -l -c "cd build-Win32-cygwin"
-C:\cygwin\bin\bash -e -l -c "build-Win32-cygwin/../configure"
-C:\cygwin\bin\bash -e -l -c "make -j4"
-C:\cygwin\bin\bash -e -l -c "make install"
+if [%Platform%] NEQ [Win32] exit 0
+
+C:\cygwin\bin\bash -e -l -c "./bootstrap.sh" || exit /B
+C:\cygwin\bin\bash -e -l -c "mkdir build-Win32-cygwin" || exit /B
+C:\cygwin\bin\bash -e -l -c "cd build-Win32-cygwin && ../configure --enable-examples-build --enable-tests-build" || exit /B
+C:\cygwin\bin\bash -e -l -c "cd build-Win32-cygwin && make -j4" || exit /B
+C:\cygwin\bin\bash -e -l -c "cd build-Win32-cygwin && make install" || exit /B
