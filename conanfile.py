1ae643251b491a2fff52fb8422f4c9028ac0a110
Tom Nelson <tom.nelson@barco.com>
2019-07-02 08:09:42 -0700

    Fixed compiler, added conan

---
+++ conanfile.py
@@ -0,0 +1,24 @@
+from conans import ConanFile, CMake
+
+class libusbConan(ConanFile):
+    name = "libusb"
+    license = "GNU Lesser General Public License v2.1"
+    url = "https://libusb.info/"
+    description = "A library for USB device access from Linux, macOS, Windows, OpenBSD/NetBSD and Haiku userspace."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {}
+    default_options = ""
+    generators = "cmake"
+    exports_sources = "*", "!build/*"
+        
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=".")
+        cmake.build()
+        cmake.install()
+
+#    def package(self):
+#         nothing to do here. All is handled by cmake.install() above.
+        
+    def package_info(self):
+        self.cpp_info.libs = ["libusb"]
