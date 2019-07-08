21cc100ec357599e1d082f78a4de3b79af8732f4
Thomas Nelson <tom.nelson@barco.com>
2019-07-08 06:38:26 -0700

    Added " in conan create

--- conan.build.cmd
+++ conan.build.cmd
@@ -1,5 +1,5 @@
 if "%APPVEYOR_REPO_TAG%" == "true" (
-    conan create %APPVEYOR_BUILD_FOLDER% "%APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare -s build_type=%configuration% -s compiler="Visual Studio" -s compiler.version=%MSVC_VERSION%
+    conan create %APPVEYOR_BUILD_FOLDER% "%APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare" -s build_type=%configuration% -s compiler="Visual Studio" -s compiler.version=%MSVC_VERSION%
     conan upload --remote barco --all %APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare
 ) else (
     if "%APPVEYOR_REPO_BRANCH%" == "barco_1.0.22" (
