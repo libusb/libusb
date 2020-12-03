libusb/libusb

commit 4a5540a925e47923ed20b4ea68aabe550dac976d

autotools: Fix a number of issues
Change the name of the project to be what it actually is called
everywhere: libusb-1.0. This allows the public libusb.h header file to
be tracked by automake through pkginclude_HEADERS.

Decouple the doc directory from automake. There aren't any targets that
automake understands, so the build uselessly recurses into the directory.
Update the makefile targets with the correct dependencies so that the
docs aren't regenerated unnecessarily. Update the doxygen config file to
include the version, exclude irrelevant source files and create the
output into 'api-1.0' instead of 'html'. Also fix a deprecation tag for
the libusb_get_port_path() function and add Solaris to the list of
supported platforms.

Fix the 'dist' target. Clean up the README file to remote the GitHub
Markdown and remove the .gitattributes file from the msvc directory.
Add doc/libusb.png to EXTRA_DIST.

Enhance the {dist,doc}-upload targets to look at the SF_USER environment
variable to get the SourceForge username. This allows maintainers (like
me!) to have a local username that is different from their SourceForge
username. Switch the docs-upload recipe to use rsync with --delete to
clean up obsolete files.

Fix the Windows shared library (DLL) targets. The dependencies for the
RC file were incorrect, leading to cases of missed recompilation. The
'all' rule should not be overridden, so define an 'all-local' rule when
necessary. Fix the rule for running dlltool on the just generated DLL so
that it only fires when the correct dependencies change and do not
bother to run the rule when not building a DLL.

Signed-off-by: Chris Dickens <christopher.a.dickens@gmail.com>

doc/Makefile.in
@@ -1,0 +1,22 @@
+LIBUSB_SRC_DIR = @top_srcdir@/libusb
+EXCLUDED_FILES = hotplug.h libusbi.h version.h version_nano.h
+LIBUSB_SRC = $(wildcard $(LIBUSB_SRC_DIR)/*.c) $(wildcard $(LIBUSB_SRC_DIR)/*.h)
+LIBUSB_DOC_SRC = $(filter-out $(addprefix $(LIBUSB_SRC_DIR)/,$(EXCLUDED_FILES)),$(LIBUSB_SRC))
+
+docs: @DOXYGEN_HTMLDIR@
+
+@DOXYGEN_HTMLDIR@: doxygen.cfg libusb.png $(LIBUSB_DOC_SRC)
+	doxygen $<
+
+docs-upload: sfurl = web.sourceforge.net:/home/project-web/libusb/htdocs
+docs-upload: @DOXYGEN_HTMLDIR@
+	if [ -z "$$SF_USER" ]; then \
+		rsync -rv --delete $< $(sfurl); \
+	else \
+		rsync -rv --delete $< $$SF_USER@$(sfurl); \
+	fi
+
+clean:
+	rm -rf @DOXYGEN_HTMLDIR@
+
+.PHONY: clean docs docs-upload

doc/doxygen.cfg.in
@@ -38,7 +38,7 @@ PROJECT_NAME           = libusb
 # could be handy for archiving the generated documentation or if some version
 # control system is used.
 
-PROJECT_NUMBER         =
+PROJECT_NUMBER         = @PACKAGE_VERSION@
 
 # Using the PROJECT_BRIEF tag one can provide an optional one line description
 # for a project that appears at the top of each page and should give viewer a
@@ -845,50 +845,7 @@ INPUT_ENCODING         = UTF-8
 # *.f, *.for, *.tcl, *.vhd, *.vhdl, *.ucf, *.qsf and *.ice.
 
 FILE_PATTERNS          = *.c \
-                         *.cc \
-                         *.cxx \
-                         *.cpp \
-                         *.c++ \
-                         *.java \
-                         *.ii \
-                         *.ixx \
-                         *.ipp \
-                         *.i++ \
-                         *.inl \
-                         *.idl \
-                         *.ddl \
-                         *.odl \
-                         *.h \
-                         *.hh \
-                         *.hxx \
-                         *.hpp \
-                         *.h++ \
-                         *.cs \
-                         *.d \
-                         *.php \
-                         *.php4 \
-                         *.php5 \
-                         *.phtml \
-                         *.inc \
-                         *.m \
-                         *.markdown \
-                         *.md \
-                         *.mm \
-                         *.dox \
-                         *.py \
-                         *.pyw \
-                         *.f90 \
-                         *.f95 \
-                         *.f03 \
-                         *.f08 \
-                         *.f \
-                         *.for \
-                         *.tcl \
-                         *.vhd \
-                         *.vhdl \
-                         *.ucf \
-                         *.qsf \
-                         *.ice
+                         *.h
 
 # The RECURSIVE tag can be used to specify whether or not subdirectories should
 # be searched for input files as well.
@@ -903,8 +860,11 @@ RECURSIVE              = NO
 # Note that relative paths are relative to the directory from which doxygen is
 # run.
 
-EXCLUDE                = @top_srcdir@/libusb/libusbi.h \
-                         @top_srcdir@/libusb/hotplug.h
+EXCLUDE                = @top_srcdir@/libusb/hotplug.h \
+                         @top_srcdir@/libusb/libusbi.h \
+                         @top_srcdir@/libusb/version.h \
+                         @top_srcdir@/libusb/version_nano.h \
+                         @top_srcdir@/libusb/os
 
 # The EXCLUDE_SYMLINKS tag can be used to select whether or not files or
 # directories that are symbolic links (a Unix file system feature) are excluded
@@ -1142,7 +1102,7 @@ GENERATE_HTML          = YES
 # The default directory is: html.
 # This tag requires that the tag GENERATE_HTML is set to YES.
 
-HTML_OUTPUT            = html
+HTML_OUTPUT            = @DOXYGEN_HTMLDIR@
 
 # The HTML_FILE_EXTENSION tag can be used to specify the file extension for each
 # generated HTML page (for example: .htm, .php, .asp).
@@ -2152,8 +2112,8 @@ INCLUDE_FILE_PATTERNS  =
 # This tag requires that the tag ENABLE_PREPROCESSING is set to YES.
 
 PREDEFINED             = API_EXPORTED= \
-                         LIBUSB_CALL= \
-                         DEFAULT_VISIBILITY=
+                         DEFAULT_VISIBILITY= \
+                         LIBUSB_CALL=
 
 # If the MACRO_EXPANSION and EXPAND_ONLY_PREDEF tags are set to YES then this
 # tag can be used to specify a list of macro names that should be expanded. The

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11469
+#define LIBUSB_NANO 11470

.gitignore
@@ -2,6 +2,7 @@
 .libs
 Makefile
 Makefile.in
+!doc/Makefile.in
 *.la
 *.lo
 *.o
@@ -39,7 +40,7 @@ examples/testlibusb
 tests/stress
 *.exe
 *.pc
-doc/html
+doc/api-1.0
 *.plg
 *.ncb
 *.opt

Makefile.am
@@ -1,10 +1,8 @@
 AUTOMAKE_OPTIONS = dist-bzip2 no-dist-gzip
 ACLOCAL_AMFLAGS = -I m4
-DISTCLEANFILES = libusb-1.0.pc
-EXTRA_DIST = TODO PORTING msvc libusb/libusb-1.0.def libusb/version_nano.h \
-  examples/getopt/getopt.c examples/getopt/getopt1.c examples/getopt/getopt.h \
-  android Xcode
-SUBDIRS = libusb doc
+EXTRA_DIST = INSTALL_WIN.txt PORTING doc/libusb.png \
+	     android msvc Xcode
+SUBDIRS = libusb
 
 if BUILD_EXAMPLES
 SUBDIRS += examples
@@ -14,15 +12,39 @@ if BUILD_TESTS
 SUBDIRS += tests
 endif
 
-pkgconfigdir=$(libdir)/pkgconfig
-pkgconfig_DATA=libusb-1.0.pc
+pkgconfigdir = $(libdir)/pkgconfig
+pkgconfig_DATA = libusb-1.0.pc
 
-.PHONY: dist-up
+# The package name is libusb-1.0, but we want the distribution
+# to be created as libusb-x.y.z instead of libusb-1.0-x.y.z
+distdir = libusb-$(VERSION)
 
-reldir = .release/$(distdir)
-dist-up: dist
+# Ensure any generated docs are cleaned out
+# We need this here because make does not recurse into doc/
+clean-local:
+	rm -rf doc/$(DOXYGEN_HTMLDIR)
+
+# Use dist-hook to accomplish the following things for the dist recipe:
+#   1) Remove the GitHub Markdown from the README file
+#   2) Remove the .gitattributes file from the msvc directory
+dist-hook:
+	chmod u+w $(distdir)/README $(distdir)/msvc
+	$(SED) -i.orig -e '/Build Status/d' $(distdir)/README
+	$(SED) -i.orig -e '/^$$/N;/^\n$$/D' $(distdir)/README
+	$(SED) -i.orig -e 's/\[\([A-Z]*\)\](\1)/\1/' $(distdir)/README
+	rm -f $(distdir)/README.orig
+	rm -f $(distdir)/msvc/.gitattributes
+
+.PHONY: dist-upload
+dist-upload: reldir = .release/$(distdir)
+dist-upload: sfurl = frs.sourceforge.net:/home/frs/project/libusb/libusb-1.0
+dist-upload: dist
 	rm -rf $(reldir)
 	mkdir -p $(reldir)
 	cp $(distdir).tar.bz2 $(reldir)
-	rsync -rv $(reldir) frs.sourceforge.net:/home/frs/project/l/li/libusb/libusb-1.0/
+	if [ -z "$$SF_USER" ]; then \
+		rsync -rv $(reldir) $(sfurl); \
+	else \
+		rsync -rv $(reldir) $$SF_USER@$(sfurl); \
+	fi
 	rm -rf $(reldir)

README.git
@@ -33,9 +33,9 @@ public account on github, if you don't have one already, and then fork a new
 libusb repository under this account from https://github.com/libusb/libusb.
 
 Then you can create a git branch for your work, that we will be able to better
-reference and test. 
+reference and test.
 
 We also suggest that, if you are planning to bring in a large development, you
 try to involve the libusb community early by letting the mailing list know, as
-you may find that other people might be eager to help you out. 
+you may find that other people might be eager to help you out.
 See http://mailing-list.libusb.info for details on how to join the mailing list.

configure.ac
@@ -15,7 +15,7 @@ LU_DEFINE_VERSION_ATOM([LIBUSB_MINOR])
 LU_DEFINE_VERSION_ATOM([LIBUSB_MICRO])
 LU_DEFINE_VERSION_RC_ATOM([LIBUSB_RC])
 
-AC_INIT([libusb],[LIBUSB_MAJOR[.]LIBUSB_MINOR[.]LIBUSB_MICRO[]LIBUSB_RC],[libusb-devel@lists.sourceforge.net],[libusb],[http://libusb.info])
+AC_INIT([libusb-1.0], [LIBUSB_MAJOR[.]LIBUSB_MINOR[.]LIBUSB_MICRO[]LIBUSB_RC], [libusb-devel@lists.sourceforge.net], [libusb-1.0], [http://libusb.info])
 
 dnl Library versioning
 dnl These numbers should be tweaked on every release. Read carefully:
@@ -103,7 +103,7 @@ case $host in
 	backend=windows
 	poll=windows
 	threads=windows
-	create_import_lib=yes
+	test "x$enable_shared" = xyes && create_import_lib=yes
 	AM_CFLAGS="${AM_CFLAGS} -fno-omit-frame-pointer"
 	;;
 *)
@@ -362,6 +362,9 @@ AM_CFLAGS="${AM_CFLAGS} -Wall -Wundef -Wunused -Wstrict-prototypes -Werror-impli
 
 AC_SUBST(AM_CFLAGS)
 AC_SUBST(LTLDFLAGS)
+
+dnl set name of html output directory for doxygen
+AC_SUBST(DOXYGEN_HTMLDIR, [api-1.0])
 
 AC_CONFIG_FILES([libusb-1.0.pc])
 AC_CONFIG_FILES([Makefile])

doc/Makefile.am
@@ -1,9 +1,0 @@
-EXTRA_DIST = doxygen.cfg.in
-
-docs: doxygen.cfg
-	doxygen $^
-
-docs-upload: docs
-	ln -s html api-1.0
-	scp -r api-1.0 web.sourceforge.net:/home/project-web/libusb/htdocs
-	rm -f api-1.0

commit 442305ec3d525bc9745cd1d7b7e89fd413595444

autotools: fix out-of-tree building of documentation
When building libusb out-of-tree, ie when running configure from another
directory, building the documentation fails with:

  $ make -C doc
  make: Entering directory '/tmp/build/doc'
  make: *** No rule to make target 'libusb.png', needed by 'api-1.0'.  Stop.
  make: Leaving directory '/tmp/build/doc'

This has likely been broken by commit 4a5540a925e4 ("autotools: Fix a number of
issues"). This patch fixes that by prefixing libusb.png with @top_srcdir@/doc.

Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

doc/Makefile.in
@@ -5,7 +5,7 @@ LIBUSB_DOC_SRC = $(filter-out $(addprefix $(LIBUSB_SRC_DIR)/,$(EXCLUDED_FILES)),
 
 docs: @DOXYGEN_HTMLDIR@
 
-@DOXYGEN_HTMLDIR@: doxygen.cfg libusb.png $(LIBUSB_DOC_SRC)
+@DOXYGEN_HTMLDIR@: doxygen.cfg @top_srcdir@/doc/libusb.png $(LIBUSB_DOC_SRC)
 	doxygen $<
 
 sfurl = web.sourceforge.net:/home/project-web/libusb/htdocs

doc/doxygen.cfg.in
@@ -51,7 +51,7 @@ PROJECT_BRIEF          = "A cross-platform user library to access USB devices"
 # pixels and the maximum width should not exceed 200 pixels. Doxygen will copy
 # the logo to the output directory.
 
-PROJECT_LOGO           = libusb.png
+PROJECT_LOGO           = @top_srcdir@/doc/libusb.png
 
 # The OUTPUT_DIRECTORY tag is used to specify the (relative or absolute) path
 # into which the generated documentation will be written. If a relative path is

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11582
+#define LIBUSB_NANO 11583