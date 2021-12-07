78774024a04b6c4c0104a5d5768f7698fba5f542
Ingvar Stepanyan <rreverser@google.com>
2021-12-07 12:53:35 +0000

    Fix license header

--- libusb/os/emscripten_webusb.cpp
+++ libusb/os/emscripten_webusb.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021 Ingvar Stepanyan <rreverser@google.com>
+ * Copyright © 2021 Google LLC
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Authors:
+ *		Ingvar Stepanyan <me@rreverser.com>
  */
 
 #include <emscripten.h>
