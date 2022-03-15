libusb/libusb

commit 6929b8270170a76e9af30d0a9e95c2081f4372b0

Fix segmentation fault in libusb_init() if usbi_backend.init() fails
If the backend init fails, the control goes to err_io_exit which tries
to clean up hotplug related lists that aren't initialized. Moving
hotplug_init before makes sure the lists are valid so if backend init
fails they get cleaned up without errors.

Closes #989

libusb/core.c
@@ -2342,13 +2342,14 @@ int API_EXPORTED libusb_init(libusb_context **ctx)
 	list_add(&_ctx->list, &active_contexts_list);
 	usbi_mutex_static_unlock(&active_contexts_lock);
 
+	usbi_hotplug_init(_ctx);
+
 	if (usbi_backend.init) {
 		r = usbi_backend.init(_ctx);
 		if (r)
 			goto err_io_exit;
 	}
 
-	usbi_hotplug_init(_ctx);
 
 	if (ctx)
 		*ctx = _ctx;

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11653
+#define LIBUSB_NANO 11654

commit 70165c73164d2b337a618c220d5dc59dfe714f89

core: Suppress hotplug events during initial enumeration
The initial enumeration should not result in hotplug events to be fired.
This is just a convenience though, API users still need to be prepared
to be notified a second time fora device that was plugged in between
libusb_init and libusb_hotplug_register_callback.

This regressed with commit 6929b8270170 ("Fix segmentation fault in
libusb_init() if usbi_backend.init() fails"). This commit avoids the
mentioned segmentation fault by avoiding to clean up the hotplug code if
it was not yet initialised.

Closes: #1082
Related: #989

libusb/core.c
@@ -2346,13 +2346,14 @@ int API_EXPORTED libusb_init(libusb_context **ctx)
 	list_add(&_ctx->list, &active_contexts_list);
 	usbi_mutex_static_unlock(&active_contexts_lock);
 
-	usbi_hotplug_init(_ctx);
-
 	if (usbi_backend.init) {
 		r = usbi_backend.init(_ctx);
 		if (r)
 			goto err_io_exit;
 	}
+
+	/* Initialize hotplug after the initial enumeration is done. */
+	usbi_hotplug_init(_ctx);
 
 	if (ctx)
 		*ctx = _ctx;

libusb/hotplug.c
@@ -171,6 +171,9 @@ void usbi_hotplug_exit(struct libusb_context *ctx)
 	if (!libusb_has_capability(LIBUSB_CAP_HAS_HOTPLUG))
 		return;
 
+	if (!usbi_atomic_load(&ctx->hotplug_ready))
+		return;
+
 	/* free all registered hotplug callbacks */
 	for_each_hotplug_cb_safe(ctx, hotplug_cb, next_cb) {
 		list_del(&hotplug_cb->list);