63d011eaaea93d7e62ae6f72b24d85272a104ebc
Sean McBride <sean@rogue-research.com>
2023-12-31 16:46:04 -0500

    WIP: Introduce (conditional) C23 fixed underlying types to enums
    I took a stab at specifying some of the sizes, based on how they are used, but it's all provisional, and there may be backwards compatibility concerns...

--- libusb/libusbi.h
+++ libusb/libusbi.h
@@ -471,7 +471,7 @@ static inline struct libusb_context *usbi_get_context(struct libusb_context *ctx
 	return ctx;
 }
 
-enum usbi_event_flags {
+enum usbi_event_flags LIBUSB_ENUM_SIZE(unsigned int) {
 	/* The list of event sources has been modified */
 	USBI_EVENT_EVENT_SOURCES_MODIFIED = 1U << 0,
 
@@ -610,7 +610,7 @@ struct usbi_transfer {
 	void *priv;
 };
 
-enum usbi_transfer_state_flags {
+enum usbi_transfer_state_flags LIBUSB_ENUM_SIZE(unsigned int) {
 	/* Transfer successfully submitted by backend */
 	USBI_TRANSFER_IN_FLIGHT = 1U << 0,
 
