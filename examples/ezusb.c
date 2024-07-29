libusb/libusb

commit 00454ab087774a62b314805b51215bba81a99aa8

examples/ezusb: Replace rewind with fseek, to check for errors
Replace rewind, which gives no error result, with fseek, which does.

Fixes all clang-tidy bugprone-unsafe-functions warnings

References #1479

examples/ezusb.c
@@ -815,7 +815,12 @@ int ezusb_load_ram(libusb_device_handle *device, const char *path, int fx_type, 
 		}
 
 		/* at least write the interrupt vectors (at 0x0000) for reset! */
-		rewind(image);
+		status = fseek(image, 0L, SEEK_SET);
+		if (ret < 0) {
+			logerror("unable to rewind file %s\n", path);
+			ret = status;
+			goto exit;
+		}
 		if (verbose)
 			logerror("2nd stage: write on-chip memory\n");
 		status = parse_ihex(image, &ctx, is_external, ram_poke);

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11913
+#define LIBUSB_NANO 11914

.clang-tidy
@@ -13,7 +13,6 @@ bugprone-*,\
 -bugprone-signed-char-misuse,\
 -bugprone-suspicious-string-compare,\
 -bugprone-switch-missing-default-case,\
--bugprone-unsafe-functions,\
 -bugprone-too-small-loop-variable,\
 clang-analyzer-*,\
 -clang-analyzer-core.NullDereference,\

commit 30ec25f738e6f4fab2e0781f452ab6144fa3b15f

examples/ezusb: Fix error checking regression in recent commit
Commit 00454ab0 accidently botched error checking when replacing
rewind() with fseek().

Thanks to GitHub user "anotheruserofgithub" for noticing.

Closes #1539

examples/ezusb.c
@@ -816,7 +816,7 @@ int ezusb_load_ram(libusb_device_handle *device, const char *path, int fx_type, 
 
 		/* at least write the interrupt vectors (at 0x0000) for reset! */
 		status = fseek(image, 0L, SEEK_SET);
-		if (ret < 0) {
+		if (status < 0) {
 			logerror("unable to rewind file %s\n", path);
 			ret = status;
 			goto exit;

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11932
+#define LIBUSB_NANO 11933