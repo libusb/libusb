15a7ebb4d426c5ce196684347d2b7cafad862626
Tormod Volden <debian.tormod@gmail.com>
2025-06-01 19:24:28 +0200

    libusb 1.0.29

--- libusb/version.h
+++ libusb/version.h
@@ -7,7 +7,7 @@
 #define LIBUSB_MINOR 0
 #endif
 #ifndef LIBUSB_MICRO
-#define LIBUSB_MICRO 28
+#define LIBUSB_MICRO 29
 #endif
 #ifndef LIBUSB_NANO
 #define LIBUSB_NANO 0
--- libusb/version_nano.h
+++ libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 11952
+#define LIBUSB_NANO 11953
