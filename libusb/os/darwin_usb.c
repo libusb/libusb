6dbb079983fcabea70ce056526be5edba9b0d943
ForestExpertise <william.root1996@gmail.com>
2026-07-22 09:35:17 -0500

    Update darwin_usb.c

--- libusb/os/darwin_usb.c
+++ libusb/os/darwin_usb.c
@@ -1055,18 +1055,24 @@ static int darwin_get_device_string(struct libusb_device *dev,
     case LIBUSB_DEVICE_STRING_MANUFACTURER:
       #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
         cf = (CFStringRef)IORegistryEntryCreateCFProperty(service, CFSTR(kUSBVendorString), kCFAllocatorDefault, 0);
-        break;
+      #else
+        cf = (CFStringRef)IORegistryEntryCreateCFProperty(service, CFSTR("USB Vendor Name"), kCFAllocatorDefault, 0);
       #endif
+      break;
     case LIBUSB_DEVICE_STRING_PRODUCT:
       #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
         cf = (CFStringRef)IORegistryEntryCreateCFProperty(service, CFSTR(kUSBProductString), kCFAllocatorDefault, 0);
-        break;
+      #else
+        cf = (CFStringRef)IORegistryEntryCreateCFProperty(service, CFSTR("USB Product Name"), kCFAllocatorDefault, 0);
       #endif
+      break;
     case LIBUSB_DEVICE_STRING_SERIAL_NUMBER:
       #if MAC_OS_X_VERSION_MIN_REQUIRED >= 1060
         cf = (CFStringRef)IORegistryEntryCreateCFProperty(service, CFSTR(kUSBSerialNumberString), kCFAllocatorDefault, 0);
-        break;
+      #else
+        cf = (CFStringRef)IORegistryEntryCreateCFProperty(service, CFSTR("USB Serial Number"), kCFAllocatorDefault, 0);
       #endif
+      break;
     case LIBUSB_DEVICE_STRING_COUNT: /* intentional fall-through, avoid -Wswitch-enum */
     default:
       IOObjectRelease(service);
