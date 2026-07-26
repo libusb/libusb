libusb/libusb

commit bc1ddf433f1fd9a1df80b04db83a058cd2bb57f9

Fix most remaining Clang -Wsign-conversion warnings
- added cast
- changed some types
- added asserts

libusb/os/events_posix.c
@@ -23,6 +23,7 @@
 
 #include "libusbi.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #ifdef HAVE_EVENTFD
@@ -258,7 +259,6 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 {
 	struct pollfd *fds = (struct pollfd *)ctx->event_data;
 	usbi_nfds_t nfds = (usbi_nfds_t)ctx->event_data_cnt;
-	int internal_fds, num_ready;
 
 	usbi_dbg(ctx, "poll() %u fds with timeout in %dms", (unsigned int)nfds, timeout_ms);
 #ifdef __EMSCRIPTEN__
@@ -269,13 +269,13 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 	 * in case they ever implement real poll. */
 	timeout_ms = 0;
 #endif
-	num_ready = poll(fds, nfds, timeout_ms);
+	int num_ready = poll(fds, nfds, timeout_ms);
 	usbi_dbg(ctx, "poll() returned %d", num_ready);
 	if (num_ready == 0) {
 		if (usbi_using_timer(ctx))
 			goto done;
 		return LIBUSB_ERROR_TIMEOUT;
-	} else if (num_ready == -1) {
+	} else if (num_ready < 0) {
 		if (errno == EINTR)
 			return LIBUSB_ERROR_INTERRUPTED;
 		usbi_err(ctx, "poll() failed, errno=%d", errno);
@@ -283,6 +283,7 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 	}
 
 	/* fds[0] is always the internal signalling event */
+	assert(nfds >= 1);
 	if (fds[0].revents) {
 		reported_events->event_triggered = 1;
 		num_ready--;
@@ -292,6 +293,7 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 
 #ifdef HAVE_OS_TIMER
 	/* on timer configurations, fds[1] is the timer */
+	assert(usbi_using_timer(ctx) ? (nfds >= 2) : 1);
 	if (usbi_using_timer(ctx) && fds[1].revents) {
 		reported_events->timer_triggered = 1;
 		num_ready--;
@@ -307,7 +309,7 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 	 * library's internal file descriptors, so we determine how many are
 	 * in use internally for this context and skip these when passing any
 	 * remaining pollfds to the backend. */
-	internal_fds = usbi_using_timer(ctx) ? 2 : 1;
+	unsigned int internal_fds = usbi_using_timer(ctx) ? 2 : 1;
 	fds += internal_fds;
 	nfds -= internal_fds;
 
@@ -334,13 +336,13 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 	}
 	usbi_mutex_unlock(&ctx->event_data_lock);
 
+	assert(num_ready >= 0);
 	if (num_ready) {
-		assert(num_ready > 0);
 		reported_events->event_data = fds;
 		reported_events->event_data_count = (unsigned int)nfds;
 	}
 
 done:
-	reported_events->num_ready = num_ready;
+	reported_events->num_ready = (unsigned int)num_ready;
 	return LIBUSB_SUCCESS;
 }

examples/ezusb.c
@@ -442,8 +442,6 @@ static int parse_iic(FILE *image, void *context,
 	int (*poke)(void *context, uint32_t addr, bool external, const unsigned char *data, size_t len))
 {
 	unsigned char data[4096];
-	uint32_t data_addr = 0;
-	size_t data_len = 0, read_len;
 	uint8_t block_header[4];
 	int rc;
 	bool external = false;
@@ -466,14 +464,14 @@ static int parse_iic(FILE *image, void *context,
 			logerror("unable to read IIC block header\n");
 			return -1;
 		}
-		data_len = (block_header[0] << 8) + block_header[1];
-		data_addr = (block_header[2] << 8) + block_header[3];
+		size_t data_len = (size_t)((block_header[0] << 8) + block_header[1]);
+		uint32_t data_addr = (uint32_t)((block_header[2] << 8) + block_header[3]);
 		if (data_len > sizeof(data)) {
 			/* If this is ever reported as an error, switch to using malloc/realloc */
 			logerror("IIC data block too small - please report this error to libusb.info\n");
 			return -1;
 		}
-		read_len = fread(data, 1, data_len, image);
+		size_t read_len = fread(data, 1, data_len, image);
 		if (read_len != data_len) {
 			logerror("read error\n");
 			return -1;

examples/fxload.c
@@ -94,7 +94,6 @@ int main(int argc, char*argv[])
 	const char *ext, *img_name[] = IMG_TYPE_NAMES;
 	int fx_type = FX_TYPE_UNDEFINED, img_type[ARRAYSIZE(path)];
 	int opt, status;
-	unsigned int i, j;
 	unsigned vid = 0, pid = 0;
 	unsigned busnum = 0, devaddr = 0, _busnum, _devaddr;
 	libusb_device *dev, **devs;
@@ -164,6 +163,7 @@ int main(int argc, char*argv[])
 
 	/* determine the target type */
 	if (type != NULL) {
+		int i;
 		for (i=0; i<FX_TYPE_MAX; i++) {
 			if (strcmp(type, fx_name[i]) == 0) {
 				fx_type = i;
@@ -190,7 +190,7 @@ int main(int argc, char*argv[])
 			logerror("libusb_get_device_list() failed: %s\n", libusb_error_name(status));
 			goto err;
 		}
-		for (i=0; (dev=devs[i]) != NULL; i++) {
+		for (unsigned int i=0; (dev=devs[i]) != NULL; i++) {
 			_busnum = libusb_get_bus_number(dev);
 			_devaddr = libusb_get_device_address(dev);
 			if ((type != NULL) && (device_path != NULL)) {
@@ -200,6 +200,7 @@ int main(int argc, char*argv[])
 			} else {
 				status = libusb_get_device_descriptor(dev, &desc);
 				if (status >= 0) {
+					unsigned int j;
 					if (verbose >= 3) {
 						logerror("examining %04x:%04x (%d,%d)\n",
 							desc.idVendor, desc.idProduct, _busnum, _devaddr);
@@ -265,7 +266,7 @@ int main(int argc, char*argv[])
 	if (verbose)
 		logerror("microcontroller type: %s\n", fx_name[fx_type]);
 
-	for (i=0; i<ARRAYSIZE(path); i++) {
+	for (unsigned int i=0; i<ARRAYSIZE(path); i++) {
 		if (path[i] != NULL) {
 			ext = path[i] + strlen(path[i]) - 4;
 			if ((libusb_strcasecmp(ext, ".hex") == 0) || (libusb_strcasecmp(ext, ".ihx") == 0))

examples/sam3u_benchmark.c
@@ -106,7 +106,7 @@ static void LIBUSB_CALL cb_xfr(struct libusb_transfer *xfr)
 		else
 			printf(" ");
 	}
-	num_bytes += xfr->actual_length;
+	num_bytes += (unsigned long)xfr->actual_length;
 	num_xfer++;
 
 	if (libusb_submit_transfer(xfr) < 0) {
@@ -133,7 +133,7 @@ static int benchmark_in(uint8_t ep)
 	if (ep == EP_ISO_IN) {
 		libusb_fill_iso_transfer(xfr, devh, ep, buf,
 				sizeof(buf), num_iso_pack, cb_xfr, NULL, 0);
-		libusb_set_iso_packet_lengths(xfr, sizeof(buf)/num_iso_pack);
+		libusb_set_iso_packet_lengths(xfr, sizeof(buf)/(unsigned int)num_iso_pack);
 	} else
 		libusb_fill_bulk_transfer(xfr, devh, ep, buf,
 				sizeof(buf), cb_xfr, NULL, 0);
@@ -160,14 +160,12 @@ static int benchmark_in(uint8_t ep)
 static void measure(void)
 {
 	struct timeval tv_stop;
-	unsigned long diff_msec;
-
 	get_timestamp(&tv_stop);
 
-	diff_msec = (tv_stop.tv_sec - tv_start.tv_sec) * 1000L;
+	long diff_msec = (tv_stop.tv_sec - tv_start.tv_sec) * 1000L;
 	diff_msec += (tv_stop.tv_usec - tv_start.tv_usec) / 1000L;
 
-	printf("%lu transfers (total %lu bytes) in %lu milliseconds => %lu bytes/sec\n",
+	printf("%lu transfers (total %lu bytes) in %lu milliseconds => %ld bytes/sec\n",
 		num_xfer, num_bytes, diff_msec, (num_bytes * 1000L) / diff_msec);
 }
 

examples/xusb.c
@@ -78,8 +78,7 @@ static void perr(char const *format, ...)
 #define CALL_CHECK(fcall) do { int _r=fcall; if (_r < 0) ERR_EXIT(_r); } while (0)
 #define CALL_CHECK_CLOSE(fcall, hdl) do { int _r=fcall; if (_r < 0) { libusb_close(hdl); ERR_EXIT(_r); } } while (0)
 #define B(x) (((x)!=0)?1:0)
-#define be_to_int32(buf) (((buf)[0]<<24)|((buf)[1]<<16)|((buf)[2]<<8)|(buf)[3])
-
+#define be_to_int32(buf) ((uint32_t)((((uint32_t)(buf)[0])<<24) | (((uint32_t)(buf)[1])<<16) | (((uint32_t)(buf)[2])<<8) | ((uint32_t)(buf)[3])))
 #define RETRY_MAX                     5
 #define REQUEST_SENSE_LENGTH          0x12
 #define INQUIRY_LENGTH                0x24
@@ -326,7 +325,7 @@ static int set_xbox_actuators(libusb_device_handle *handle, uint8_t left, uint8_
 }
 
 static int send_mass_storage_command(libusb_device_handle *handle, uint8_t endpoint, uint8_t lun,
-	uint8_t *cdb, uint8_t direction, int data_length, uint32_t *ret_tag)
+	uint8_t *cdb, uint8_t direction, uint32_t data_length, uint32_t *ret_tag)
 {
 	static uint32_t tag = 1;
 	uint8_t cdb_len;
@@ -548,12 +547,12 @@ static int test_mass_storage(libusb_device_handle *handle, uint8_t endpoint_in, 
 	cdb[8] = 0x01;	// 1 block
 
 	send_mass_storage_command(handle, endpoint_out, lun, cdb, LIBUSB_ENDPOINT_IN, block_size, &expected_tag);
-	libusb_bulk_transfer(handle, endpoint_in, data, block_size, &size, 5000);
+	libusb_bulk_transfer(handle, endpoint_in, data, (int)block_size, &size, 5000);
 	printf("   READ: received %d bytes\n", size);
 	if (get_mass_storage_status(handle, endpoint_in, expected_tag) == -2) {
 		get_sense(handle, endpoint_in, endpoint_out);
 	} else {
-		display_buffer_hex(data, size);
+		display_buffer_hex(data, (unsigned int)size);
 		if (binary_dump) {
 			fd = fopen(binary_name, "w");
 			if (fd != NULL) {

libusb/libusb.h
@@ -1525,7 +1525,7 @@ struct libusb_transfer {
 
 	/** Actual length of data that was transferred. Read-only, and only for
 	 * use within transfer callback function. Not valid for isochronous
-	 * endpoint transfers. */
+	 * endpoint transfers. Never negative. */
 	int actual_length;
 
 	/** Callback function. This will be invoked when the transfer completes,

commit c1a0ea5ac1a4f0af599727a4a78d29c58e5ee4ca

core: keep event fd declaration C++ compatible
Declare internal_fds before the existing goto paths while preserving its
unsigned type and assignment point. The C++ CI added by #1845 exposed this
regression from #1730.

Assisted-by: codex:GPT-5.6-Sol

libusb/os/events_posix.c
@@ -259,6 +259,7 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 {
 	struct pollfd *fds = (struct pollfd *)ctx->event_data;
 	usbi_nfds_t nfds = (usbi_nfds_t)ctx->event_data_cnt;
+	unsigned int internal_fds;
 
 	usbi_dbg(ctx, "poll() %u fds with timeout in %dms", (unsigned int)nfds, timeout_ms);
 #ifdef __EMSCRIPTEN__
@@ -309,7 +310,7 @@ int usbi_wait_for_events(struct libusb_context *ctx,
 	 * library's internal file descriptors, so we determine how many are
 	 * in use internally for this context and skip these when passing any
 	 * remaining pollfds to the backend. */
-	unsigned int internal_fds = usbi_using_timer(ctx) ? 2 : 1;
+	internal_fds = usbi_using_timer(ctx) ? 2 : 1;
 	fds += internal_fds;
 	nfds -= internal_fds;
 